c02786d1bea630814286180460018fb1c30b6cd6
Joost Boomkamp <3604126+increddibelly@users.noreply.github.com>
2021-12-06 14:49:27 +0100

    remove pretty LanternFish model :-(

--- 2021/joostboomkamp/AdventOfCode/AOCTests/Day06 LanternFish.cs
+++ 2021/joostboomkamp/AdventOfCode/AOCTests/Day06 LanternFish.cs
@@ -13,48 +13,6 @@ public class Day06_LanternFish
     }
 
     [Test]
-    public void Test_Day06_Example()
-    {
-        // arrange
-        var input = InputDay06.Example;
-        var cut = new School2(input);
-
-        // act
-        cut.Tick();
-
-        // assert
-        cut.ToString().Should().Be("After    1 days: 2, 3, 2, 0, 1");
-    }
-
-    [Test]
-    public void Test_Day06_Spawn()
-    {
-        // arrange
-        var input = InputDay06.Example;
-        var cut = new School2(input);
-
-        // act
-        cut.Tick(4);
-
-        // assert
-        cut.ToString().Should().Be("After    4 days: 6, 0, 6, 4, 5, 6, 7, 8, 8");
-    }
-
-    [Test]
-    public void Test_Day06_Spawn_18()
-    {
-        // arrange
-        var input = InputDay06.Example;
-        var cut = new School2(input);
-
-        // act
-        cut.Tick(18);
-
-        // assert
-        cut.ToString().Should().Be("After   18 days: 6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8, 8, 8");
-    }
-
-    [Test]
     public void Test_Day06_80Days()
     {
         // arrange
@@ -93,7 +51,7 @@ public class Day06_LanternFish
         cut.Tick(255);
 
         // assert
-        cut.Count.Should().Be(1525126772942L);//26984457539L);
+        cut.Count.Should().Be(1525126772942L);
     }
 
     [Test]
